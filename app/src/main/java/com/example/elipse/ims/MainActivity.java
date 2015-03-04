216b259c31a3f1c18410aadc660757a174c823f3
hsaidi <saidi.houssem@hotmail.com>
2015-03-04 13:21:54 +0100

    MAJ Projet

--- app/src/main/java/com/example/elipse/ims/MainActivity.java
+++ app/src/main/java/com/example/elipse/ims/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.elipse.ims;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#000000"));
+        Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bg);
+        canvas.drawRect(50, 50, 200, 200, paint);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.rect);
+
+
+        int sdk = android.os.Build.VERSION.SDK_INT;
+        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            ll.setBackgroundDrawable(new BitmapDrawable(bg));
+        } else {
+            ll.setBackground(new BitmapDrawable(getResources(), bg));
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
